b9a76d67c5725b2106f94059be350b2f9fd7848f
Abed Halawi <halawi.abed@gmail.com>
2017-10-20 15:13:10 +0300

    debugging build hooks

--- run_tests.sh
+++ run_tests.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo "=> Installing dependencies"
+curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
 composer install --prefer-dist --no-interaction
 
 echo "=> running tests"
