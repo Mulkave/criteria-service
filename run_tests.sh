a6ee62768ba4b39290f4470063ea18c3031234b3
Abed Halawi <halawi.abed@gmail.com>
2017-10-20 15:05:18 +0300

    debugging build hooks

--- run_tests.sh
+++ run_tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "=> Installing dependencies"
-php composer.phar install --prefer-dist --no-interaction
+composer install --prefer-dist --no-interaction
 
 echo "=> running tests"
 /code/vendor/bin/phpunit
