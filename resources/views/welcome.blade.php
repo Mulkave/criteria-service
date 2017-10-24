b1890e526da3a6e6aa77a74743ef0a94373a5699
Abed Halawi <halawi.abed@gmail.com>
2017-10-24 22:04:16 +0300

    change welcome page - testing pushing changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -75,7 +75,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Criteria Service
+                    Criteria Service - You are welcome!
                 </div>
             </div>
         </div>
